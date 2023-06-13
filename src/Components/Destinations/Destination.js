bac2dabce094d930f16dddc70284f06518b6ae25
nishant25062002 <mainwalnishant9@gmail.com>
2023-06-13 12:10:51 +0530

    Update footer and destination.jsx

--- src/Components/Destinations/Destination.js
+++ src/Components/Destinations/Destination.js
@@ -8,10 +8,10 @@ import StarRoundedIcon from '@mui/icons-material/StarRounded';
 import { useHistory } from "react-router-dom";
 
 const Destination = (props) => {
-  
+
   const history = useHistory();
   const goToPage = (loc) => {
-    history.push({pathname:`/${loc}`,state:{page_loc:0}});
+    history.push({ pathname: `/${loc}`, state: { page_loc: 0 } });
   }
 
   return (
@@ -25,14 +25,10 @@ const Destination = (props) => {
       <div className="slider_splideOut">
         <div className="slider-splide">
           <Splide className='slider_splide_react' options={{ rewind: true, perPage: 4 }} aria-label="React Splide Example">
-            {/* <Splide options={ { rewind: true,perPage:4} } aria-label="React Splide Example"> */}
-            {/* MODIFIED CARD : According to the design */}
-            {props?.trendingTreks?.length>0 && props?.trendingTreks?.map(trek => {
-
-              console.log(trek);
+            {props?.trendingTreks?.length > 0 && props?.trendingTreks?.map(trek => {
               return (
                 <SplideSlide onClick={() => goToPage(trek?.trek_id)} className='slider_splide_react1'>
-                  <div style={{backgroundImage: `url(${trek.trek_data?.img1 || "/Images/aboutus-bg.png"})` }} className='slider_splide_react_img'>
+                  <div style={{ backgroundImage: `url(${trek.trek_data?.img1 || "/Images/aboutus-bg.png"})` }} className='slider_splide_react_img'>
                     <p className='trek__cursive'>{trek?.trek_id}</p>
                   </div>
                   <div className="one1">
@@ -47,88 +43,10 @@ const Destination = (props) => {
                 </SplideSlide>
               );
             })}
-            {/*
-      <SplideSlide className='slider_splide_react1'>
-        <img className='slider_splide_react_img' src="/Images/b.png" alt="Image 2"/>
-        <div className="one1">
-            <p className="head">Auli</p>
-            <p>3D/2N</p>
-            <div className='star_text'><img src="/Images/1.svg"></img><p>4.9/5</p></div>
-            <p>₹ 3,999/-  </p>
-            <p>₹ 6,999/-  </p>
-        </div>
-      </SplideSlide>
 
-      <SplideSlide className='slider_splide_react1'>
-        <img className='slider_splide_react_img' src="/Images/c.png" alt="Image 2"/>
-        <div className="one1">
-            <p className="head">Chopta </p>
-            <p>3D/2N</p>
-            <div className='star_text'><img src="/Images/1.svg"></img><p>4.9/5</p></div>
-            <p>₹ 3,999/-  </p>
-            <p>₹ 6,999/-  </p>
-        </div>
-      </SplideSlide>
-      <SplideSlide className='slider_splide_react1'>
-        <img className='slider_splide_react_img' src="/Images/d.png" alt="Image 2"/>
-        <div className="one1">
-            <p className="head">Rishikesh</p>
-            <p>4D/3N</p>
-            <div className='star_text'><img src="/Images/1.svg"></img><p>4.9/5</p></div>
-            <p>₹ 2,499/-</p>
-            <p>₹ 4,999/-</p>
-        </div>
-      </SplideSlide>
-      <SplideSlide className='slider_splide_react1'>
-        <img className='slider_splide_react_img' src="/Images/a.png" alt="Image 1"/>
-        <div className="one1">
-            <p className="head">Auli - Chopta</p>
-            <p>4D/3N</p>
-            <div className='star_text'><img src="/Images/1.svg"></img><p>4.9/5</p></div>
-            <p>₹ 4,499/- </p>
-            <p>₹ 7,499/- </p>
-        </div>
-      </SplideSlide>
-      <SplideSlide className='slider_splide_react1'>
-        <img className='slider_splide_react_img' src="/Images/b.png" alt="Image 2"/>
-        <div className="one1">
-            <p className="head">Auli</p>
-            <p>3D/2N</p>
-            <div className='star_text'><img src="/Images/1.svg"></img><p>4.9/5</p></div>
-            <p>₹ 3,999/-  </p>
-            <p>₹ 6,999/-  </p>
-        </div>
-      </SplideSlide>
-
-      <SplideSlide className='slider_splide_react1'>
-        <img className='slider_splide_react_img' src="/Images/c.png" alt="Image 2"/>
-        <div className="one1">
-            <p className="head">Chopta </p>
-            <p>3D/2N</p>
-            <div className='star_text'><img src="/Images/1.svg"></img><p>4.9/5</p></div>
-            <p>₹ 3,999/-  </p>
-            <p>₹ 6,999/-  </p>
-        </div>
-      </SplideSlide>
-      <SplideSlide className='slider_splide_react1'>
-        <img className='slider_splide_react_img' src="/Images/d.png" alt="Image 2"/>
-        <div>
-            <p className="head">Rishikesh</p>
-            <p>4D/3N</p>
-            <img src="/Images/1.svg"></img>
-            <p>4.9/5</p>
-            <p>₹ 2,499/-</p>
-            <p>₹ 4,999/-</p>
-        </div>
-      </SplideSlide>
-        */}
           </Splide>
-          {/* for mobile views */}
           <Splide className='slider_splide_react_mobile' options={{ rewind: true, perPage: 2 }} aria-label="React Splide Example">
-            {/* <Splide options={ { rewind: true,perPage:4} } aria-label="React Splide Example"> */}
             {props.trendingTreks.map(trek => {
-
-              // console.log(trek.trek_id);
               return (
                 <SplideSlide className='slider_splide_react1'>
                   <div style={{ backgroundImage: `url(${trek.trek_data?.img1 || "/Images/aboutus-bg.png"})` }} className='slider_splide_react_img'>
@@ -146,88 +64,6 @@ const Destination = (props) => {
                 </SplideSlide>
               );
             })}
-            {/* <SplideSlide className='slider_splide_react1'>
-        <img className='slider_splide_react_img' src="/Images/a.png" alt="Image 1"/>
-        <div className="one1">
-            <p className="head">Auli - Chopta</p>
-            <p>4D/3N</p>
-            <div className='star_text'><img src="/Images/1.svg"></img><p>4.9/5</p></div>
-            <p>₹ 4,499/- </p>
-            <p>₹ 7,499/- </p> 
-        </div>
-      </SplideSlide>
-      <SplideSlide className='slider_splide_react1'>
-        <img className='slider_splide_react_img' src="/Images/b.png" alt="Image 2"/>
-        <div className="one1">
-            <p className="head">Auli</p>
-            <p>3D/2N</p>
-            <div className='star_text'><img src="/Images/1.svg"></img><p>4.9/5</p></div>
-            <p>₹ 3,999/-  </p>
-            <p>₹ 6,999/-  </p>
-        </div>
-      </SplideSlide>
-
-      <SplideSlide className='slider_splide_react1'>
-        <img className='slider_splide_react_img' src="/Images/c.png" alt="Image 2"/>
-        <div className="one1">
-            <p className="head">Chopta </p>
-            <p>3D/2N</p>
-            <div className='star_text'><img src="/Images/1.svg"></img><p>4.9/5</p></div>
-            <p>₹ 3,999/-  </p>
-            <p>₹ 6,999/-  </p>
-        </div>
-      </SplideSlide>
-      <SplideSlide className='slider_splide_react1'>
-        <img className='slider_splide_react_img' src="/Images/d.png" alt="Image 2"/>
-        <div className="one1">
-            <p className="head">Rishikesh</p>
-            <p>4D/3N</p>
-            <img src="/Images/1.svg"></img>
-            <p>4.9/5</p>
-        </div>
-      </SplideSlide>
-      <SplideSlide className='slider_splide_react1'>
-        <img className='slider_splide_react_img' src="/Images/a.png" alt="Image 1"/>
-        <div className="one1">
-            <p className="head">Auli - Chopta</p>
-            <p>4D/3N</p>
-            <div className='star_text'><img src="/Images/1.svg"></img><p>4.9/5</p></div>
-            {/* <p>₹ 4,499/- </p>
-            <p>₹ 7,499/- </p> 
-        </div>
-      </SplideSlide>
-      <SplideSlide className='slider_splide_react1'>
-        <img className='slider_splide_react_img' src="/Images/b.png" alt="Image 2"/>
-        <div className="one1">
-            <p className="head">Auli</p>
-            <p>3D/2N</p>
-            <div className='star_text'><img src="/Images/1.svg"></img><p>4.9/5</p></div>
-            {/* <p>₹ 3,999/-  </p>
-            <p>₹ 6,999/-  </p> 
-        </div>
-      </SplideSlide>
-
-      <SplideSlide className='slider_splide_react1'>
-        <img className='slider_splide_react_img' src="/Images/c.png" alt="Image 2"/>
-        <div className="one1">
-            <p className="head">Chopta </p>
-            <p>3D/2N</p>
-            <div className='star_text'><img src="/Images/1.svg"></img><p>4.9/5</p></div>
-            {/* <p>₹ 3,999/-  </p>
-            <p>₹ 6,999/-  </p> 
-        </div>
-      </SplideSlide>
-      <SplideSlide className='slider_splide_react1'>
-        <img className='slider_splide_react_img' src="/Images/d.png" alt="Image 2"/>
-        <div className="one1">
-            <p className="head">Rishikesh</p>
-            <p>4D/3N</p>
-            <div className='star_text'><img src="/Images/1.svg"></img><p>4.9/5</p></div>
-            {/* <p>₹ 2,499/-</p>
-            <p>₹ 4,999/-</p> 
-        </div>
-      </SplideSlide>
-        */}
           </Splide>
         </div>
       </div>
--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -25,9 +25,9 @@ const Footer = () => {
   }
 
   return (
-    <div className="footer-boundary">
-      <div className="footer-container">
-        <div className="Thrillo-div">
+    <div className="footer_boundary">
+      <div className="footer_container">
+        <div className="Thrillo_div">
           <h1>THRILLOZEAL</h1>
           <img src="/Images/thrillozeal_logo_mobile.png"/>
           <p>
@@ -40,26 +40,26 @@ const Footer = () => {
         </div>
       
       <div className="combo__link_contact">
-      <div className="Links-div">
-          <h1>USEFUL LINKS</h1>
+      <div className="Links_div">
+          <h1>LINKS</h1>
           <ul>
             <li onClick={() => goToPage("",0)}>Home</li>
             <li onClick={ () => document.getElementById('destination').scrollIntoView({ behavior: "smooth"})} >Trending </li>
             <li onClick={() => goToPage("about",0)}>About Us</li>
           </ul>
         </div>
-        <div className="Contact-div">
+        <div className="Contact_div">
           <h1>CONTACT US</h1>
-          <span className="contact-span">
+          <span className="contact_span">
             <PhoneInTalkOutlinedIcon/>
             <p>+91 83030 22306</p>
           </span>
-          <span className="contact-span">
+          <span className="contact_span">
             <LocationOnOutlinedIcon/>
             <p >B 36, Nehru Colony, Dehrdun
               Uttarakhand, India 248001</p>
           </span>
-          <span className="contact-span">
+          <span className="contact_span">
             <EmailOutlinedIcon/>
             <p>info@thrillozeal.com</p>
           </span>
@@ -81,7 +81,7 @@ const Footer = () => {
               list</li>
             <li className="newsletter_li">We respect your
               privacy</li>
-            <form className="footer-form">
+            <form className="footer_form">
               <input id="email_id" onChange={(e) => setEmail(e.target.value)} type="text" placeholder="you@awesome.com"></input>
               {messageToggle && <span className="thankyou__span">Thank you for subscribing to Thrillozeal Newsletter!</span>}
               {/* <span className="thankyou__span">Thank you for subscribing to Thrillozeal Newsletter!</span> */}
@@ -95,6 +95,9 @@ const Footer = () => {
         <img onClick={() => window.open("https://www.instagram.com/thrillozeal/")} src="/Images/Instagram.png"></img>
         <img onClick={() => goToPage("",0)} src="/Images/Twitter Circled.png"></img>
         <img onClick={() => goToPage("",0)} src="/Images/LinkedIn.png"></img>
+      </div>
+      <div className='socials'>
+          copyright@thrillozeal.com
       </div>
     </div>
   );
