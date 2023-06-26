1fd8a47162e00e01e7860d8fb9eae855751cb0ef
nishant25062002 <mainwalnishant9@gmail.com>
2023-06-26 17:17:43 +0530

    Update some changes

--- src/home/Add.jsx
+++ src/home/Add.jsx
@@ -1,12 +1,31 @@
-import React from 'react';
-import './Adds.css'
+import { useEffect, useRef, useState } from "react";
+import "./Adds.css";
 
 function Add() {
+  const [shown, setShown] = useState(false);
+  const [height, setHeight] = useState(1000);
+  const loaderRef = useRef(null);
+
+  useEffect(() => {
+    if (!shown) {
+      const windowHeight = window.innerHeight;
+      window.addEventListener("scroll", () => {
+        const { top } = loaderRef?.current?.getBoundingClientRect();
+        setHeight(top);
+      });
+      console.log(height);
+      if (height <= windowHeight) {
+        console.log("true hogya ");
+        setShown(true);
+      }
+    }
+  }, [height]);
+
   return (
-    <div className='Adds'>
-        <img src="/Images/add.webp" alt=""/>
+    <div ref={loaderRef} className={`Adds ${shown && "addAnimation"}`}>
+      <img src="/Images/add.webp" alt="" />
     </div>
-  )
+  );
 }
 
-export default Add
+export default Add;
